6301d73192d9787c9cbe7977451168c6c96f1094
Aryan Hardik Dani <daniaryan212@gmail.com>
2025-04-23 00:14:50 +0530

    feat: add gh-pages deployment and update package.json
    - Added "gh-pages" as a dependency for deploying to GitHub Pages.
    - Updated scripts in package.json to include predeploy and deploy commands.
    - Set the homepage in package.json to the GitHub Pages URL.
    - Updated vite.config.js to set the base path for GitHub Pages.
    - Created a new GitHub Actions workflow for deploying static content to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set the base path for GitHub Pages
+  base: "/surveyproject/",
   plugins: [react()],
 });
